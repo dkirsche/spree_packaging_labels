eb5b30481713707e57b1e45882beb4cdf252a53b
dkirsche <dan@blinkbeverage.com>
2015-06-10 20:23:34 -0500

    filter and render mutli-label orders

--- app/controllers/spree/admin/labels_controller.rb
+++ app/controllers/spree/admin/labels_controller.rb
@@ -1,7 +1,8 @@
 class Spree::Admin::LabelsController < Spree::Admin::BaseController
 	def index
-        puts "got here"
-        @orders=Spree::Order.all
+        puts YAML::dump(params)
+        @orders=Spree::Order.where(:number => params[:order_number])
+        @items_per_label=5
         render layout: "layout1"
   	end
 end
